124d0fc03533ccfa48ff8110957b52edf7c27190
JoaoVictor087 <joaovictoralves087@gmail.com>
2026-03-24 09:24:26 -0300

    First Commit

---
+++ src/main/java/joaovictor/TesteBackendJava/TesteBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package joaovictor.TesteBackendJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteBackendJavaApplication.class, args);
+	}
+
+}
